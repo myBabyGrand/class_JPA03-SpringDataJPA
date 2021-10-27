e539b562b6d44b364c4307b4fbb8e0db8cc2c14e
mybabygrand <amuro_ray@kakao.com>
2021-10-27 23:18:15 +0900

    Section 1. Setting Project Environment. finished.

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,36 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @Rollback(value = false)
+    void save() {
+        Member member = new Member("TestMember");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+
+    @Test
+    void find() {
+    }
+}
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("Member를 통해서 SpringData JPA가 잘 동작하는지 확인해보자")
+    public void testMember(){
+        Member member = new Member("TestMember2");
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUserName()).isEqualTo(savedMember.getUserName());
+        assertThat(findMember).isEqualTo(savedMember);
+
+    }
+}
